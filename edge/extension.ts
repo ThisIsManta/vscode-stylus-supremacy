28f2ab9b03c8065c5624dd6861b83a1068d713ff
Anantachai Saothong (Manta) <thisismanta@outlook.com>
2017-04-16 00:09:15 +0700

    Added ability to format partial content (cont')

--- edge/extension.ts
+++ edge/extension.ts
@@ -1,5 +1,7 @@
+import * as fs from 'fs'
+import * as fp from 'path'
 import * as vscode from 'vscode'
-import { format } from 'stylus-supremacy'
+import { format, createFormattingOptionsFromStylintOptions } from 'stylus-supremacy'
 
 const defaultFormattingOptions = require('stylus-supremacy/edge/defaultFormattingOptions.json')
 
@@ -17,24 +19,77 @@ class Formatter implements vscode.DocumentFormattingEditProvider, vscode.Documen
         }
     }
 
-    format(document: vscode.TextDocument, documentOptions: vscode.FormattingOptions, cancellationToken: vscode.CancellationToken, range?: vscode.Range, resumeOnError: boolean = false): vscode.ProviderResult<vscode.TextEdit[]> {
-        const formattingOptions = {
-            ...this.workspaceFormattingOptions,
-            tabStopChar: documentOptions.insertSpaces ? ' '.repeat(documentOptions.tabSize) : '\t',
-            newLineChar: document.eol === vscode.EndOfLine.CRLF ? '\r\n' : '\n',
+    provideDocumentFormattingEdits(document: vscode.TextDocument, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
+        return this.format(document, options, token)
+    }
+
+    provideDocumentRangeFormattingEdits(document: vscode.TextDocument, range: vscode.Range, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
+        return this.format(document, options, token, range, true)
+    }
+
+    private findStylintOptions(document: vscode.TextDocument): object {
+        let stylintPath = null
+        if (!document.isUntitled && document.fileName.startsWith(vscode.workspace.rootPath)) {
+            // Find `.stylintrc` starting from the current viewing document up to the working directory
+            const pathList = document.fileName.substring(vscode.workspace.rootPath.length).split(/(\\|\/)/).filter(path => path.length > 0)
+            pathList.pop() // Remove the file name
+            while (pathList.length >= 0) {
+                const workPath = fp.join(vscode.workspace.rootPath, ...pathList, '.stylintrc')
+                if (fs.existsSync(workPath)) {
+                    stylintPath = workPath
+                    break
+                }
+                pathList.pop()
+            }
+
+        } else {
+            // Find `.stylintrc` in the working directory
+            const workPath = fp.join(vscode.workspace.rootPath, '.stylintrc')
+            if (fs.existsSync(workPath) === false) {
+                stylintPath = workPath
+            }
         }
 
-        try {
-            const result = format(document.getText(range), formattingOptions)
+        if (stylintPath) {
+            try {
+                const stylintOptions = JSON.parse(fs.readFileSync(stylintPath, 'utf-8'))
+                return createFormattingOptionsFromStylintOptions(stylintOptions)
+            } catch (ex) {
+                console.error(ex)
+            }
+        }
 
-            if (result.warnings.length > 0 && resumeOnError === false) {
+        return {}
+    }
+
+    private format(document: vscode.TextDocument, documentOptions: vscode.FormattingOptions, cancellationToken: vscode.CancellationToken, range?: vscode.Range, ignoreErrors: boolean = false): vscode.ProviderResult<vscode.TextEdit[]> {
+        const formattingOptions = {
+            ...this.workspaceFormattingOptions,
+            ...this.findStylintOptions(document),
+            tabStopChar: documentOptions.insertSpaces ? ' '.repeat(documentOptions.tabSize) : '\t',
+            newLineChar: document.eol === vscode.EndOfLine.CRLF ? '\r\n' : '\n',
+            wrapMode: !!range // true for partial formatting, otherwise false
+        }
+        console.log(formattingOptions);
+
+        try {
+            // Extend the selection range
+            if (range && range.start.line !== range.end.line) {
+                range = new vscode.Range(range.start.line, 0, range.end.line, Number.MAX_VALUE)
+            }
+
+            const content = document.getText(range)
+            const result = format(content, formattingOptions)
+            
+            // Show a warning dialog
+            if (result.warnings.length > 0 && ignoreErrors === false) {
                 vscode.window.showWarningMessage(result.warnings[0].message)
                 result.warnings.forEach(warn => {
                     console.warn(warn.message)
                 })
             }
 
-            if (cancellationToken.isCancellationRequested) {
+            if (cancellationToken.isCancellationRequested || result.text.length === 0) {
                 return null
             } else if (range) {
                 return [vscode.TextEdit.replace(range, result.text)]
@@ -43,7 +98,7 @@ class Formatter implements vscode.DocumentFormattingEditProvider, vscode.Documen
             }
 
         } catch (ex) {
-            if (resumeOnError === false) {
+            if (ignoreErrors === false) {
                 vscode.window.showWarningMessage(ex.message)
                 console.error(ex)
             }
@@ -51,27 +106,23 @@ class Formatter implements vscode.DocumentFormattingEditProvider, vscode.Documen
             return null
         }
     }
-
-    provideDocumentFormattingEdits(document: vscode.TextDocument, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
-        return this.format(document, options, token)
-    }
-
-    provideDocumentRangeFormattingEdits(document: vscode.TextDocument, range: vscode.Range, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
-        return this.format(document, options, token, range, true)
-    }
 }
 
 const formatter = new Formatter()
 
 export function activate(context: vscode.ExtensionContext) {
+    // Initialize settings
     formatter.configure()
 
+    // Update settings
     context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(() => {
         formatter.configure()
     }))
 
+    // Subscribe "format document" event
     context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider('stylus', formatter))
 
+    // Subscribe "format selection" event
     context.subscriptions.push(vscode.languages.registerDocumentRangeFormattingEditProvider('stylus', formatter))
 }
 
