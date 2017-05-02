dc0fdb3a23c0e3f2665a1e2e7f24470aafc1ccb9
Anantachai Saothong (Manta) <thisismanta@outlook.com>
2017-05-02 19:58:09 +0700

    Amended expanding selection

--- edge/extension.ts
+++ edge/extension.ts
@@ -46,9 +46,11 @@ class Formatter implements vscode.DocumentFormattingEditProvider, vscode.Documen
 
     private findStylintOptions(document: vscode.TextDocument): object {
         let stylintPath = null
+
+        // Skip if there is no working directories (anonymous window)
         if (vscode.workspace.rootPath !== undefined) {
             if (document.isUntitled === false && document.fileName.startsWith(vscode.workspace.rootPath)) {
-                // Find `.stylintrc` starting from the current viewing document up to the working directory
+                // Find `.stylintrc` starting from the current active document up to the working directory
                 const pathList = document.fileName.substring(vscode.workspace.rootPath.length).split(/(\\|\/)/).filter(path => path.length > 0)
                 pathList.pop() // Remove the file name
                 while (pathList.length >= 0) {
@@ -71,6 +73,7 @@ class Formatter implements vscode.DocumentFormattingEditProvider, vscode.Documen
             }
         }
 
+        // Read `.stylintrc` and convert it to the standard formatting options
         if (stylintPath) {
             try {
                 const stylintOptions = JSON.parse(fs.readFileSync(stylintPath, 'utf-8'))
@@ -89,57 +92,89 @@ class Formatter implements vscode.DocumentFormattingEditProvider, vscode.Documen
             ...this.findStylintOptions(document),
             tabStopChar: documentOptions.insertSpaces ? ' '.repeat(documentOptions.tabSize) : '\t',
             newLineChar: document.eol === vscode.EndOfLine.CRLF ? '\r\n' : '\n',
-            wrapMode: !!originalRange // true for partial formatting, otherwise false
+            wrapMode: !!originalRange // True for partial formatting, otherwise false
         }
 
         try {
-            // Extend the selection range
-            let extendedRange: vscode.Range = originalRange
+            let modifiedRange: vscode.Range
+            let insertNewLineAtFirst = false
+            let insertNewLineAtLast = false
             if (!originalRange) {
-                extendedRange = document.validateRange(new vscode.Range(0, 0, Number.MAX_VALUE, Number.MAX_VALUE))
+                // Expand the selection to cover the whole file
+                modifiedRange = new vscode.Range(0, 0, Number.MAX_VALUE, Number.MAX_VALUE)
 
             } else if (originalRange.start.line !== originalRange.end.line) {
+                // Expand the selection to match the nearest closing braces
                 let start: vscode.Position
                 if (document.getText(new vscode.Range(originalRange.start.line, originalRange.start.character, originalRange.start.line + 1, 0)).trim().length === 0) {
                     start = new vscode.Position(originalRange.start.line + 1, 0)
+
                 } else {
-                    start = new vscode.Position(originalRange.start.line, 0)
+                    const remainingText = document.getText(new vscode.Range(originalRange.start.line, 0, originalRange.start.line, originalRange.start.character))
+                    if (remainingText.includes('}')) {
+                        start = new vscode.Position(originalRange.start.line, remainingText.lastIndexOf('}') + 1)
+                        insertNewLineAtFirst = true
+
+                    } else {
+                        start = new vscode.Position(originalRange.start.line, 0)
+                    }
                 }
 
                 let end: vscode.Position
-                if (originalRange.end.character === 0) {
+                if (document.lineAt(originalRange.end.line).text.substring(originalRange.end.character).trim().charAt(0) === '}') {
+                    end = new vscode.Position(originalRange.end.line, document.lineAt(originalRange.end.line).text.indexOf('}', originalRange.end.character) + 1)
+                    insertNewLineAtLast = true
+
+                } else if (originalRange.end.character === 0) {
                     end = new vscode.Position(originalRange.end.line - 1, Number.MAX_VALUE)
+
                 } else {
-                    end = new vscode.Position(originalRange.end.line, Number.MAX_VALUE)
-                }
+                    const remainingText = document.lineAt(originalRange.end.line).text
+                    if (remainingText.substring(0, originalRange.end.character).includes('}')) {
+                        end = new vscode.Position(originalRange.end.line, remainingText.indexOf('}') + 1)
+                        insertNewLineAtLast = true
 
-                extendedRange = document.validateRange(new vscode.Range(start, end))
-                if (extendedRange.isEmpty) {
-                    return null
-                }
+                    } else if (remainingText.substring(originalRange.end.character).includes('}')) {
+                        end = new vscode.Position(originalRange.end.line, remainingText.indexOf('}', originalRange.end.character) + 1)
+                        insertNewLineAtLast = true
 
-                /*if (extendedRange.isEqual(originalRange) === false) {
-                    const oldSelection = vscode.window.activeTextEditor.selection
-                    let newSelection: vscode.Selection
-                    if (oldSelection.anchor.isBefore(oldSelection.active)) {
-                        newSelection = new vscode.Selection(extendedRange.start, extendedRange.end)
                     } else {
-                        newSelection = new vscode.Selection(extendedRange.end, extendedRange.start)
+                        end = new vscode.Position(originalRange.end.line, Number.MAX_VALUE)
                     }
-                    vscode.window.activeTextEditor.selection = newSelection
-                }*/
+                }
+
+                modifiedRange = new vscode.Range(start, end)
+
+            } else {
+                // Use the original selection
+                modifiedRange = originalRange
             }
-            
-            const inputContent = document.getText(extendedRange)
+
+            // Stop processing if the selection range is not valid
+            modifiedRange = document.validateRange(modifiedRange)
+            if (modifiedRange.isEmpty) {
+                return null
+            }
+
+            // Stop processing if the input content is empty
+            const inputContent = document.getText(modifiedRange)
             if (inputContent.trim().length === 0) {
                 return null
             }
 
             const outputContent = format(inputContent, formattingOptions)
-            if (cancellationToken && cancellationToken.isCancellationRequested || outputContent.length === 0) {
+
+            // Stop processing if it is cancelled or the output content is empty
+            if (cancellationToken && cancellationToken.isCancellationRequested === true || outputContent.length === 0) {
                 return null
+
             } else {
-                return [vscode.TextEdit.replace(extendedRange, outputContent)]
+                // Replace with the output content
+                return [vscode.TextEdit.replace(modifiedRange,
+                    (insertNewLineAtFirst ? formattingOptions.newLineChar : '') +
+                    outputContent +
+                    (insertNewLineAtLast ? formattingOptions.newLineChar : '')
+                )]
             }
 
         } catch (ex) {
