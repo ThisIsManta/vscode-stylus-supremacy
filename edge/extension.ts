0ce24a8da47f7f958ca31d56aa6ff48c4b718359
Anantachai Saothong (Manta) <thisismanta@outlook.com>
2017-09-04 12:48:44 +0700

    Added support of JSON with comments for ".stylintrc" file

--- edge/extension.ts
+++ edge/extension.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs'
 import * as fp from 'path'
 import * as vscode from 'vscode'
 import { format, createFormattingOptions, createFormattingOptionsFromStylint } from 'stylus-supremacy'
+import * as JSONWithComments from 'comment-json'
 
 class Formatter implements vscode.DocumentFormattingEditProvider, vscode.DocumentRangeFormattingEditProvider, vscode.OnTypeFormattingEditProvider {
     private workspaceFormattingOptions: object
@@ -76,7 +77,7 @@ class Formatter implements vscode.DocumentFormattingEditProvider, vscode.Documen
         // Read `.stylintrc` and convert it to the standard formatting options
         if (stylintPath) {
             try {
-                const stylintOptions = JSON.parse(fs.readFileSync(stylintPath, 'utf-8'))
+                const stylintOptions = JSONWithComments.parse(fs.readFileSync(stylintPath, 'utf-8'), null, true)
                 return createFormattingOptionsFromStylint(stylintOptions)
             } catch (ex) {
                 console.error(ex)
