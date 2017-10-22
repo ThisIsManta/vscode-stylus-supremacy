276dc4f10b6d8adcced22f4fb15af9eb896f6829
Anantachai Saothong (Manta) <thisismanta@outlook.com>
2017-10-22 14:32:53 +0700

    Updated stylus-supremacy engine

--- edge/removeStylusSupremacyFromPackageLock.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-
-if (fs.existsSync('./package-lock.json')) {
-	const packageLock = JSON.parse(fs.readFileSync('./package-lock.json'))
-	delete packageLock.dependencies['stylus-supremacy']
-	fs.writeFileSync('./package-lock.json', JSON.stringify(packageLock, null, 2), 'utf-8')
-}
